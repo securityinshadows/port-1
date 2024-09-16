f6bdea4123d0ca7fda2daee541e23120be6417eb
Ayoub, W. <166329193+securityinshadows@users.noreply.github.com>
2024-09-16 19:22:25 +0000

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// Dark mode function
+function myFunction() {
+ var element = document.body;
+ var toggleSwitch = document.querySelector(".switch input"); // Select the input of the switch
+ var label = document.querySelector(".switch .slider");
+    
+// Toggle dark mode
+    element.classList.toggle("dark-mode");
+}  
